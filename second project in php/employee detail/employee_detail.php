e708e3ca5c9e46887299d0bf6ae617fc7bcf00e7
Sanjaya Dahal <109066146+SanjayaDahal@users.noreply.github.com>
2022-11-03 15:38:26 +0545

    Add files via upload

---
+++ second project in php/employee detail/employee_detail.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>employee details </title>
+</head>
+<body>
+    <h2> <u>employee details</u></h2>
+       <table>
+        <tr>
+            <td><label for="employee id">employee id:</label></td>
+            <td><input type="text"="username" id = "username" placeholder="enter username ..."></td>
+        </tr> 
+        <tr>
+            <td><label for=" employee name">employee name:</label></td>
+            <td><input type="text"="employee name" id = "employee name" placeholder="enter employee name ..."></td>
+        </tr> 
+        <tr>
+            <td><label for=" employee address">employee address:</label></td>
+            <td><input type="text"="employee address" id = "employee address" placeholder="enter employee address ..."></td>
+        </tr> 
+        <tr>
+            <td><label for=" email address">email:</label></td>
+            <td><input type=" email"="email address" id = "email address" placeholder="enter email address ..."></td>
+        </tr> 
+        <tr>
+            <td><label for="contact no">contact number:</label></td>
+            <td><input type=" int"="contact no" number = "contact no" placeholder="enter contact no ..."></td>
+        </tr> 
+        <tr>
+            <td><label for="employee DOB">employee DOB:</label></td>
+            <td><input type="date"="employee DOB" number = "employee DOB" placeholder="enter date..."></td>
+        </tr> 
+        <tr>
+            <td><label for="employee basic salary">employee basic salary:</label></td>
+            <td><input type=" int"="employee basic salary" basic salary = "employee basic salary" placeholder="enter basic salary ..."></td>
+        </tr> 
+        <tr>
+            <td> &nbsp;</td>
+            <td><input type ="submit"name="SUBMIT"></td>
+        </tr>
+</body>
+</html>
---
+++ second project in php/working with get method/WorkingWithPHPGetMethod.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>working with get method</title>
+</head>
+<body>
+    <h2> simple loginn system</h2>
+    <table>
+        <form method="get"action="process.php">
+        <tr>
+            <td><label for="employee id">employee id:</label></td>
+            <td><input type="text"="username" id = "username" placeholder="enter username ..."></td>
+        </tr> 
+        <tr>
+            <td><label for=" employee name">employee name:</label></td>
+            <td><input type="text"="employee name" id = "employee name" placeholder="enter employee name ..."></td>
+        </tr> 
+        <tr>
+            <td><label for=" employee address">employee address:</label></td>
+            <td><input type="text"="employee address" id = "employee address" placeholder="enter employee address ..."></td>
+        </tr> 
+        <tr>
+            <td><label for=" email address">email:</label></td>
+            <td><input type=" email"="email address" id = "email address" placeholder="enter email address ..."></td>
+        </tr> 
+        <tr>
+            <td><label for="contact no">contact number:</label></td>
+            <td><input type=" int"="contact no" number = "contact no" placeholder="enter contact no ..."></td>
+        </tr> 
+        <tr>
+            <td><label for="employee DOB">employee DOB:</label></td>
+            <td><input type="date"="employee DOB" number = "employee DOB" placeholder="enter date..."></td>
+        </tr> 
+        <tr>
+            <td><label for="employee basic salary">employee basic salary:</label></td>
+            <td><input type=" int"="employee basic salary" basic salary = "employee basic salary" placeholder="enter basic salary ..."></td>
+        </tr> 
+        <tr>
+            <td> &nbsp;</td>
+            <td><input type ="submit"name="SUBMIT"></td>
+        </tr>
+</table>
+</body>
+</html>
